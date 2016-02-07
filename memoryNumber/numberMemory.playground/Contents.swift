fd4d1b63862e014adcd081333004b4bb0a7bd641
Daniel Henares <daniel.henares@icloud.com>
2016-02-07 21:32:18 +0100

    PLayground

---
+++ memoryNumber/numberMemory.playground/Contents.swift
@@ -0,0 +1,36 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+var numeros = 0...100
+
+
+
+for id in numeros{
+    
+    
+    if(id % 5 == 0)
+    {
+        
+        
+        print( id,  "Bingo!!!");
+        
+    }
+    
+    if (id % 2 == 0)
+    {
+        print( id,  "par!!!");
+    }
+    else
+    {
+        print(id, "impar!!!");
+    }
+    
+    if( id >= 30 && id <= 40){
+        
+        print( id,  "viva swift!!!");
+    }
+    
+    
+}
